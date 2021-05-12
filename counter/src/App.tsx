c263e91ae6f1dbb36ab79186cda0a2ef12ae70b3
buzmakovTim <buzmakov1987@gmail.com>
2021-05-12 13:28:20 -0700

    Counter almos all done

--- counter/src/App.tsx
+++ counter/src/App.tsx
@@ -1,38 +1,91 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { DisplayWindow } from './Components/DisplayWindow/DisplayWindow';
 
 function App() {
   
-  
+ 
+  //Save Data in local Storage
+  const saveMaxValueToLocalStorage = (newTopCountValue: number, newStartCountValue: number) => {
+    setTopCount(newTopCountValue)
+    setStartCount(newStartCountValue)
+    localStorage.setItem('topValue', JSON.stringify(newTopCountValue))
+    localStorage.setItem('startValue', JSON.stringify(newStartCountValue))
+    
+  }
+
+  let [topInputValue, setTopInputValue] = useState<number>(0) // Value for Top Input
+  let [startInputValue, setStartInputValue] = useState<number>(0) // Value for Max Input
   let [counter, setCount] = useState<number>(0)
-  let [topCounter, setTopCount] = useState<number>(10)
+  let [topCount, setTopCount] = useState<number>(0)
+  let [startCount, setStartCount] = useState<number>(0)
+  
 
   
-  let inctementFunc = () => {
-    setCount(counter = counter + 1)
+
+  useEffect(() => {
+    let getFromLocalStorage = 0
+    let stringFromLocalStorage  = localStorage.getItem('topValue')
+     if(stringFromLocalStorage)
+    {
+      getFromLocalStorage = JSON.parse(stringFromLocalStorage)
+    }
+    setTopInputValue(getFromLocalStorage)
+    setTopCount(getFromLocalStorage)
+}, [topCount])
+
+  // Function to Set Top Value in settings window
+  let setTopValueFunc = (newValue: number) => {
+    if(newValue >= 0) {
+      setTopInputValue(newValue)
+    }
+    if(newValue < startInputValue){
+      setStartInputValue(newValue)
+    }
+    
+  }
+  // Function to Set Start Value in settings window
+  let setStartValueFunc = (newValue: number) => {
+    if(newValue >= 0) {
+      setStartInputValue(newValue)
+      if(newValue > topInputValue){
+        setTopInputValue(newValue)
+      }     
+    }
+    
+  }
+  
+  let incrementFunc = () => {
+    setCount(counter + 1)
   }
   let resetCounter = () => {
-    setCount(0)
+    setCount(startCount)
   }
 
   
   return (
     <div className="App">
       <div>
+        {/* Settings Window */}
         <DisplayWindow isSettings={true}
-                       counter={counter} 
-                       incFunction={inctementFunc} 
+                       counter={counter}  
                        resetFunction={resetCounter}
-                       topCount={topCounter}/>
+                       topCount={topCount}
+                       startInputValue={startInputValue}
+                       topInputValue={topInputValue}
+                       setTopValueFunc={setTopValueFunc}
+                       setStartValueFunc={setStartValueFunc}
+                       saveMaxValueToLocalStorage={saveMaxValueToLocalStorage}/>
+                       
       </div>
       <div>
+        {/* Counter Window */}
         <DisplayWindow isSettings={false}
                        counter={counter} 
-                       incFunction={inctementFunc} 
+                       incFunction={incrementFunc} 
                        resetFunction={resetCounter}
-                       topCount={topCounter}/>
+                       topCount={topCount}/>
       </div>
       
 
--- counter/src/Components/DisplayWindow/DisplayWindow.tsx
+++ counter/src/Components/DisplayWindow/DisplayWindow.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
+import React, { ChangeEvent } from 'react';
 import { Button } from '../Button/Button';
 import s from './DisplayWindow.module.css';
 
@@ -9,19 +10,62 @@ type DisplayWindowPropsType = {
     resetFunction?: () => void
     setFunction?: () => void
     isSettings: boolean
+    topInputValue?: number
+    startInputValue?: number
+    setTopValueFunc?: (newValue: number) => void
+    setStartValueFunc?: (newValue: number) => void
+    saveMaxValueToLocalStorage?: (newTopCountValue: number, newStartCountValue: number) => void
 }
 
 export function DisplayWindow(props: DisplayWindowPropsType) {
 
-    let incDisabled: boolean = false
+    let incDisabled: boolean
     if(props.counter === props.topCount){
         incDisabled = true
+    } else {
+        incDisabled = false
     }
+
+
     let resetDisabled: boolean = false
     if(props.counter === 0){
         resetDisabled = true
     }  
     
+    let setMaxValueFuncHandler = (e: ChangeEvent<HTMLInputElement>) => {    
+        if(props.setTopValueFunc)
+        {
+            props.setTopValueFunc(parseInt(e.currentTarget.value))
+        }
+    }
+
+    let setStartValueFuncHandler = (e: ChangeEvent<HTMLInputElement>) => {    
+        if(props.setStartValueFunc)
+        {
+            props.setStartValueFunc(parseInt(e.currentTarget.value))
+        }
+    }
+
+    let saveToLocalStorageHandler = () => {
+        let topVal = 0;
+        let startVal = 0;
+        
+        if(props.saveMaxValueToLocalStorage){
+            
+            if(props.topInputValue){
+                if(props.topInputValue != 0){
+                    topVal = props.topInputValue
+                }
+            }
+            if(props.startInputValue){
+                if(props.startInputValue != 0){
+                    startVal = props.startInputValue
+                }
+            }       
+            props.saveMaxValueToLocalStorage(topVal, startVal)
+        }
+    }
+
     //Screen for Settings
     let settingsScreen = <div className={s.mainScreenSettings}>
                             {/* Title */}
@@ -31,8 +75,8 @@ export function DisplayWindow(props: DisplayWindowPropsType) {
                             </div>
                             {/* Inputs */}
                             <div>
-                            <div><input type="text" /></div>
-                            <div><input type="text" /></div> 
+                            <div><input type="number" onChange={setMaxValueFuncHandler} value={props.topInputValue}/></div>
+                            <div><input type="number" onChange={setStartValueFuncHandler} value={props.startInputValue}/></div> 
                             </div>
                             
                         </div>
@@ -45,7 +89,7 @@ export function DisplayWindow(props: DisplayWindowPropsType) {
 
     // Button For Setting
     let settingsButtons = <div className={s.buttonScreen}>
-                        <Button title={'Set'} callBack={props.incFunction} isDisable={incDisabled}/>
+                        <Button title={'Set'} callBack={saveToLocalStorageHandler} isDisable={false}/>
                         </div>
         
     // Buttons For Counter
